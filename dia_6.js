b5bc9218e12bc61dc7fdb9f6a480de197a4fec9e
jorgegago <jorgegago2526@gmail.com>
2022-12-06 23:49:41 +0100

    dia_6_v2_ok

--- dia_6.js
+++ dia_6.js
@@ -11,28 +11,40 @@ function createCube(size) {
 
     // variable que retornaremos
     let res = ''
+    let superior = []
+    let inferior = []
 
     // formamos los lados el cubo segun su tamaño
     const ladoSup = lateralSup.repeat(size)
     const ladoInf = lateralInf.repeat(size)
     let frontal = ''
+    let frontalInf = ''
     let espacios = ''
 
     //creamos la mitad superior del cubo
     for (let i = 1; i <= size; i++) {
         espacios = ' '.repeat(size - i)
         frontal = sup.repeat(i)
-        res +=`${espacios}${frontal}${ladoSup}\n`
+        frontalInf = inf.repeat(size - (i - 1))
+        superior.push(`${espacios}${sup.repeat(i)}${ladoSup}`)
+        inferior.push(`${' '.repeat((size - 1) - (size - i))}${frontalInf}${ladoInf}`)
+        superior.push('\n')
+        inferior.push('\n')
     }
+    inferior = inferior.slice(0,-1)
+    
+    superior = superior.join('')
+    inferior = inferior.join('')
+    res = `${superior}${inferior}`
 
     //creamos la mitad inferior del cubo
-    for (let i = size; i > 0; i--) {
-        espacios = ' '.repeat(size - i)
-        frontal = inf.repeat(i)
-        res +=`${espacios}${frontal}${ladoInf}\n`
-    }
+    // for (let i = size; i > 0; i--) {
+    //     espacios = ' '.repeat(size - i)
+    //     frontal = inf.repeat(i)
+    //     res +=`${espacios}${frontal}${ladoInf}\n`
+    // }
 
-    res = res.slice(0, -1)
+    // res = res.slice(0, -1)
 
   return res
 }
